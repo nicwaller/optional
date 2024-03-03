4b5e39ba65179c11455f6930f91e8c377b915b79
Nic Waller <code@nicwaller.com>
2024-03-03 09:19:15 -0800

    try again

--- optional.go
+++ optional.go
@@ -5,8 +5,8 @@ import (
 )
 
 //goland:noinspection GoUnusedExportedFunction
-func NewOptional[T any](ptr *T) wrapper.Wrapper[T] {
-	return wrapper.New[T](ptr)
+func NewOptional[T any](ptr *T) Optional[T] {
+	return Optional[T](wrapper.New[T](ptr))
 }
 
 type Optional[T any] wrapper.Wrapper[T]
